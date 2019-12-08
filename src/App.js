7653ba2e04adba413fad6495e5a8c1854a6d86c9
Armin Basirian <ar.basirian@gmail.com>
2019-12-08 20:32:16 +0330

    Shop page design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/home-page/home-page.component';
-import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shop/hats" component={HatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,7 @@ import { withRouter } from 'react-router-dom';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div onClick={() => history.push(`${match.url}${linkUrl}`)} className={`${size} menu-item`}>
-        <div
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }} 
-            className="background-image"
-            ></div>
+        <div style={{ backgroundImage: `url(${imageUrl})`}} className="background-image"></div>
         <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">SHOP NOW</span>
