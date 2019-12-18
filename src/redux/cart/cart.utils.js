1199c346f9263a1bc5d29e5fca2dd6112abe75e1
Armin <ar.basirian@gmail.com>
2019-12-18 13:03:13 +0330

    Actions on checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,10 +4,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
     if(existingCartItem) {
         return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem 
-
+            cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
         )
     }
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 } ]
-}
+};
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+    return cartItems.filter(
+        cartItem => cartItem.id !== cartItemToClear.id
+    );
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const selectedItem =  cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+    if(selectedItem && selectedItem.quantity > 1) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity -1 } : cartItem
+        )
+    }else {
+        return clearItemFromCart(cartItems, selectedItem);
+    }
+};
